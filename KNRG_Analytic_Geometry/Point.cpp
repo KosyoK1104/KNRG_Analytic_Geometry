aa20b48bb7693e6698db52bea95b32301f6ca24b
KosyoK1104 <kosyok1104@gmail.com>
2021-05-11 21:47:20 +0300

    Point first initialization

--- KNRG_Analytic_Geometry/Point.cpp
+++ KNRG_Analytic_Geometry/Point.cpp
@@ -1,1 +1,19 @@
 #include "Point.h"
+
+Point::Point() :x(0), y(0), z(0) {};
+
+void Point::setX(double x) { this->x = x; };
+void Point::setY(double y) { this->y = y; };
+void Point::setZ(double z) { this->z = z; };
+
+double Point::getX() const{
+	return x;
+}
+
+double Point::getY() const {
+	return y;
+}
+
+double Point::getZ() const {
+	return z;
+}
--- KNRG_Analytic_Geometry/Point.h
+++ KNRG_Analytic_Geometry/Point.h
@@ -1,7 +1,25 @@
 #pragma once
 #include "Element.h"
-class Point :
-    public Element
+#ifndef Point_h
+#define Point_h
+
+class Point : public Element
 {
+public:
+	Point();
+	~Point();
+
+	 void setX(double x);
+	 void setY(double y);
+	 void setZ(double z);
+
+	 double getX() const;
+	 double getY() const;
+	 double getZ() const;
+
+private:
+	double x, y, z;
 };
+
+#endif
 
