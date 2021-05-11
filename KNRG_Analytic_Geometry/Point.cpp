92e03ac7451b485c510dd4943289780085d10eb6
KosyoK1104 <kosyok1104@gmail.com>
2021-05-11 22:15:49 +0300

    Point init

--- KNRG_Analytic_Geometry/Point.cpp
+++ KNRG_Analytic_Geometry/Point.cpp
@@ -1,1 +1,40 @@
 #include "Point.h"
+
+Point::Point() :x(0), y(0), z(0) {};
+
+Point::Point(double x, double y, double z) :x(x), y(y), z(z) {};
+
+Point::Point(const Point& p) {
+	if (this != &p) {
+		x = p.getX();
+		y = p.getY();
+		z = p.getZ();
+	}
+}
+
+Point
+
+void Point::setX(double x) { 
+	this->x = x; 
+};
+
+void Point::setY(double y) { 
+	this->y = y; 
+};
+
+void Point::setZ(double z) { 
+	this->z = z; 
+};
+
+
+double Point::getX() const{
+	return x;
+}
+
+double Point::getY() const {
+	return y;
+}
+
+double Point::getZ() const {
+	return z;
+}
--- KNRG_Analytic_Geometry/Point.h
+++ KNRG_Analytic_Geometry/Point.h
@@ -1,7 +1,38 @@
 #pragma once
+
+#ifndef Point_h
+#define Point_h
+
 #include "Element.h"
-class Point :
-    public Element
+#include <iostream>
+
+class Point : public Element
 {
+public:
+	Point();
+	Point(double x, double y, double z);
+	Point(const Point& p);
+	Point& operator=(const Point& p);
+	~Point();
+
+	bool operator==(const Point& p);
+
+	 void setX(double x);
+	 void setY(double y);
+	 void setZ(double z);
+
+	 double getX() const;
+	 double getY() const;
+	 double getZ() const;
+
+	 std::ostream& ins(std::ostream& out);
+	 std::istream& ext(std::istream& in) const;
+
+private:
+	double x, y, z;
 };
 
+std::ostream& operator<<(std::ostream& lhs, Point& rhs);
+std::istream& operator>>(std::istream& lhs, Point& rhs);
+
+#endif
