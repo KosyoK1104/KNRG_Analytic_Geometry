17b5d581140918981e6ed48c8a0310406fa69abc
Nikola S <thehighonelev@gmail.com>
2021-05-20 18:43:53 +0300

    Menu / Paralel / Semi fix

--- KNRG_Analytic_Geometry/Menu.cpp
+++ KNRG_Analytic_Geometry/Menu.cpp
@@ -253,11 +253,16 @@ void ExecuteVectorOperations()
 			cout << "\n";
 			break;
 		}
+
+		// DO TUKA SICHKO BACHKA NE BARAI
+
 		case 5:
 		{
 			system("cls");
-			int opt;
+			int opt1, opt2;
 			cout << "Please choose two vectors to check if they are parallel";
+			cin >> opt1 >> opt2;
+			dynamic_cast<Vector*>(elementsArr[opt1])->paralelVector(dynamic_cast<Vector*>(elementsArr[opt2]));
 			break;
 		}
 		case 6:
